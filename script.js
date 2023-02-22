f36409f9f8dc8cc0ae306e11ecf45c29bb0eaf7f
MM <maryse.mongeau@cegepoutaouais.qc.ca>
2023-02-22 14:23:05 -0500

    Solution partielle

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 let aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
+
+$("button").click(function (){
+    let $nb = $("#essai").val();
+    if (+$nb === aleatoire){
+        $("#messages").text("Bravo");
+    }
+    else if(+$nb > aleatoire){
+        $("#messages").text("Trop grand");
+        effacerNombres(+$nb, 100);
+        $("#essais").append($nb + " ");
+    }
+    else {
+        $("#messages").text("Trop petit");
+        effacerNombres(0, +$nb);
+        $("#essais").append($nb + " ");
+    }
+});
+
+function effacerNombres(min, max){
+    for (let i = min; i <= max; i++){
+        $("#"+i).hide();
+    }
+}
